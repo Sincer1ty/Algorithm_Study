0d3e9a748afa948a439065065d2092f344a28eb4
Sincer1ty <87846251+Sincer1ty@users.noreply.github.com>
2024-08-14 14:05:05 +0900

    Update 김성희 가운데를 말해요

--- homework/3. 1655 가운데를 말해요/김성희.py
+++ homework/3. 1655 가운데를 말해요/김성희.py
@@ -3,22 +3,25 @@ input = sys.stdin.readline
 
 import heapq
 
-minq = [10001]
-maxq = [0]
+minq = []
+maxq = []
 
 N = int(input())
 for i in range(N):
     num = int(input())
     
-    heapq.heappush(maxq, -num)
+    if len(maxq) == len(minq):
+        heapq.heappush(maxq, -num)
+    else:
+        heapq.heappush(minq, num)
     
-    if minq[0] < maxq[0]*-1:
+    if minq and maxq and minq[0] < maxq[0]*-1:
         min = heapq.heappop(maxq) * -1
         max = heapq.heappop(minq)
         heapq.heappush(minq, min)
         heapq.heappush(maxq, max * -1)
         
-    if len(minq) + 1 < len(maxq):
-        heapq.heappush(minq, heapq.heappop(maxq) * -1)
+    # if len(minq) + 1 < len(maxq):
+    #     heapq.heappush(minq, heapq.heappop(maxq) * -1)
 
     print(maxq[0]*-1)
