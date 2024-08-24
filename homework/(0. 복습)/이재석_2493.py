a2beb3a45142d606212e9a23530564a2451d5635
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-08-24 13:33:28 +0900

    1st commit

---
+++ homework/(0. 복습)/이재석_2493.py
@@ -0,0 +1,18 @@
+import sys
+
+read = sys.stdin.readline
+
+n = int(read().strip())
+bld_list = list(map(int,read().strip().split()))
+
+stack = []
+for i in range(n):
+    if stack and stack[-1][0] > bld_list[i]:
+        max_bld = stack.pop()
+        print(max_bld[1]+1)
+        print(stack[-1][1]+1)
+
+    else:
+        print(0)
+
+    stack.append([bld_list[i], i])
