f622d25415c48fcd4898b0c362301e0d6128d416
jsonsvalue <54315562+jsonsvalue@users.noreply.github.com>
2024-08-24 14:47:26 +0900

    Update 이재석_크게만들기2812.py

--- homework/(24.08.22) 2812 크게 만들기/이재석_크게만들기2812.py
+++ homework/(24.08.22) 2812 크게 만들기/이재석_크게만들기2812.py
@@ -1,15 +1,19 @@
 import sys
 read = sys.stdin.readline
-n, era = map(int,read().strip().split)
-give_num = read().strip()
 
+n, k = map(int,read().strip().split())
+give_num = read().strip()
 
 stack = []
 for i in range(len(give_num)):
-    # stack.append(give_num[i])
-    while stack and stack[-1] < give_num[i] and era > 0:
+    # print(give_num[i])
+    while stack and stack[-1] < give_num[i] and k>0:
         stack.pop()
-        era -= 1
+        k-=1
     stack.append(give_num[i])
-    
+
+if k>0:
+    stack = stack[:-k]
+
+print(''.join(stack))
 
