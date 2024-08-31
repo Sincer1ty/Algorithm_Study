6a85c6f126adfe8095028ea5e807c7113aa970ae
seula <urghb12375@gmail.com>
2024-08-31 14:42:16 +0900

    Update 김슬아.py
    통과된 코드 추가합니다

--- 24.08/( 08.30 ) 3055 탈출/김슬아.py
+++ 24.08/( 08.30 ) 3055 탈출/김슬아.py
@@ -1,3 +1,53 @@
+# 완성코드 추가
+from collections import deque
+import sys
+input=sys.stdin.readline
+
+R,C=map(int,input().split())
+forest=[list(input().strip()) for _ in range(R)]
+
+hedgehog=deque()
+water=deque()
+
+for i in range(R):
+    for j in range(C):
+        if forest[i][j]=="S":
+            hedgehog.append((i,j,0))
+        elif forest[i][j] =="*":
+            water.append((i,j))
+dx=[1,-1,0,0]
+dy=[0,0,1,-1]
+def bfs():
+    while hedgehog:
+        # 물확장
+        for _ in range(len(water)):
+            x,y=water.popleft()
+            for i in range(4):
+                nx,ny = x+dx[i],y+dy[i]
+                if 0<=nx<R and 0<=ny<C and (forest[nx][ny]=="." or forest[nx][ny]=="S"):
+                    forest[nx][ny]="*"
+                    water.append((nx,ny))
+        # 고슴도치 이동
+        for _ in range(len(hedgehog)):
+            x,y,time =hedgehog.popleft()
+            # 굴 만나면 while문 빠져나오고 종료
+            if forest[x][y]=="D":
+                return time
+            for i in range(4):
+                nx,ny = x+dx[i],y+dy[i]
+                if 0<=nx<R and 0<=ny<C:
+                    if forest[nx][ny]=="." or forest[nx][ny]=="D":
+                        if forest[nx][ny]!="D":
+                            forest[nx][ny]="S"
+                        hedgehog.append((nx,ny,time+1))
+
+    return "KAKTUS"
+
+print(bfs())
+    
+
+
+
 # 미완성코드입니다..
 from collections import deque
 import sys
