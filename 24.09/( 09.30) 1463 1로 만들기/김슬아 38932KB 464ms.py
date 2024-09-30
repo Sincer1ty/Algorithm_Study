358fb9bfe91ff6f38837e24ce5f1679c9e70f3e8
seula <urghb12375@gmail.com>
2024-09-30 12:33:02 +0900

    Create 김슬아 38932KB 464ms.py

---
+++ 24.09/( 09.30) 1463 1로 만들기/김슬아 38932KB 464ms.py
@@ -0,0 +1,50 @@
+# 내가 처음에 푼 코드
+import sys
+N=int(input())
+input= sys.stdin.readline
+dp=[float("inf")]*(N+1)
+for i in range(1,N+1):
+    if i == 1 : 
+        dp[i]=0
+    elif i%2 ==0 and i%3 ==0:
+        dp[i]=min(1+dp[i//2],1+dp[i//3])
+    elif i%2 ==0:
+        dp[i]=min(1+dp[i//2],dp[i-1]+1)
+    elif i%3 ==0:
+        dp[i]=min(1+dp[i//3],dp[i-1]+1)
+    else:
+        dp[i] = dp[i-1]+1
+
+print(dp[N])
+# 클로드가 내꺼 최적화 해 준 코드
+import sys
+N=int(input())
+input= sys.stdin.readline
+dp=[0]*(N+1)
+for i in range(1,N+1):
+    if i == 1 : 
+        dp[i]=0
+    elif i%2 ==0 and i%3 ==0:
+        dp[i]=min(dp[i//2],dp[i//3])+1
+    elif i%2 ==0:
+        dp[i]=min(dp[i//2],dp[i-1])+1
+    elif i%3 ==0:
+        dp[i]=min(+dp[i//3],dp[i-1])+1
+    else:
+        dp[i] = dp[i-1]+1
+
+print(dp[N])
+# 클로드가 최적화 해준 코드인데 내꺼보다 더 느림
+import sys
+N=int(input())
+input= sys.stdin.readline
+dp=[0]*(N+1)
+for i in range(2,N+1):
+    dp[i] = dp[i-1]+1
+    if i%2 ==0:
+        dp[i]=min(1+dp[i//2],dp[i])
+    if i%3 ==0:
+        dp[i]=min(1+dp[i//3],dp[i])
+        
+
+print(dp[N])
