0c788aefc259ea16ffbd7dabe46997b7cdcf3b2f
Hutosuto <biomatrix117@naver.com>
2024-10-02 13:40:53 +0900

    dd

--- 24.10/( 10.02 ) 2098 외판원 순회/고태환 답지봄.py
+++ 24.10/( 10.02 ) 2098 외판원 순회/고태환 답지봄.py
@@ -1,20 +1,32 @@
 import sys
-input=sys.stdin.readline
+imput=sys.stdin.readline
+
+# 도시개수
 n=int(input())
-w=[list(map(int,input().split())) for _ in range(n)]
-dp=[[0]*(1<<n)  for _ in range(n) ]
-total=(1<<n)-1
-def tsp(curr,visited):
-    if visited==total:
-        if w[curr][0]==0:
-            return 1e10
+# 비용 행렬
+w=[]
+
+for i in range(n):
+    w.append([])
+    w[i]=list(map(int,input().split()))
+
+d=[[0 for j in range(1<<16)] for i in range(16)] # n(1~16)
+
+def tsp(c,v):
+    if v==(1<<n)-1: # 모든노드 방문
+        if w[c][0] ==0: # 시작도시로 들어갈 수 없음
+            return float('inf')
         else:
-            return w[curr][0]
-    if dp[curr][visited]!=0:
-        return dp[curr][visited]
-    dp[curr][visited]=1e10
-    for i in range(1,n):
-        if not visited &(1<<i) and w[curr][i]!=0:
-            dp[curr][visited]=min(dp[curr][visited], tsp(i,visited|1<<i)+w[curr][i])
-    return dp[curr][visited]
+            return w[c][0]
+
+    if d[c][v]!=0: # 이미방문
+        return d[c][v]
+
+    res = float('inf')
+    for i in range(0,n):
+        if (v&(1<<i))==0 and w[c][i]!=0:  # 방문한 적없고, 갈수 있는 도시
+            res = min(res, tsp(i, (1<<i))+w[c][i])
+    d[c][v] = res
+    return d[c][v]
+
 print(tsp(0,1))
